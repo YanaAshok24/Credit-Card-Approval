301aff5fac3749b3181de868b01f3e1c4825d65d
YanaAshok24 <57429507+YanaAshok24@users.noreply.github.com>
2022-11-18 18:26:03 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,219 @@
+feature_set_1 = [
+    {
+        'id': 'CNT_CHILDREN',
+        'name': 'Children'
+    },
+    {
+        'id': 'AMT_INCOME_TOTAL',
+        'name': 'Total Income'
+    },
+    {
+        'id': 'FLAG_WORK_PHONE',
+        'name': 'Has Work Phone'
+    },
+    {
+        'id': 'FLAG_PHONE',
+        'name': 'Has Phone'
+    },
+    {
+        'id': 'FLAG_EMAIL',
+        'name': 'Has Email'
+    },
+    {
+        'id': 'CNT_FAM_MEMBERS',
+        'name': 'Family Members'
+    },
+    {
+        'id': 'age_months',
+        'name': 'Age in Months'
+    },
+    {
+        'id': 'employment_months',
+        'name': 'Employment Months'
+    },
+    {
+        'id': 'CODE_GENDER_F',
+        'name': 'Is Female'
+    },
+    {
+        'id': 'CODE_GENDER_M',
+        'name': 'Is Male'
+    },
+    {
+        'id': 'FLAG_OWN_CAR_N',
+        'name': 'Has No Own Car'
+    },
+    {
+        'id': 'FLAG_OWN_CAR_Y',
+        'name': 'Has Own Car'
+    },
+    {
+        'id': 'FLAG_OWN_REALTY_N',
+        'name': 'Has No Realty'
+    }, 
+    {
+        'id': 'FLAG_OWN_REALTY_Y',
+        'name': 'Has Realty'
+    },
+]
+
+feature_set_2 = [
+    {
+        'id': 'NAME_INCOME_TYPE_Commercial associate',
+        'name': 'Commercial associate'
+    },
+    {
+        'id': 'NAME_INCOME_TYPE_Pensioner',
+        'name': 'Pensioner'
+    },
+    {
+        'id': 'NAME_INCOME_TYPE_State servant',
+        'name': 'State servant'
+    },
+    {
+        'id': 'NAME_INCOME_TYPE_Working',
+        'name': 'Working'
+    },
+    {
+        'id': 'NAME_EDUCATION_TYPE_Academic degree',
+        'name': 'Academic degree'
+    },
+    {
+        'id': 'NAME_EDUCATION_TYPE_Higher education',
+        'name': 'Higher education'
+    },
+    {
+        'id': 'NAME_EDUCATION_TYPE_Incomplete higher',
+        'name': 'Incomplete higher'
+    },
+    {
+        'id': 'NAME_EDUCATION_TYPE_Lower secondary',
+        'name': 'Lower secondary'
+    },
+    {
+        'id': 'NAME_EDUCATION_TYPE_Secondary / secondary special',
+        'name': 'Secondary / secondary special'
+    },
+    {
+        'id': 'NAME_FAMILY_STATUS_Civil marriage',
+        'name': 'Civil marriage'
+    },
+    {
+        'id': 'NAME_FAMILY_STATUS_Married',
+        'name': 'Married'
+    },
+    {
+        'id': 'NAME_FAMILY_STATUS_Separated',
+        'name': 'Separated'
+    },
+    {
+        'id': 'NAME_FAMILY_STATUS_Single / not married',
+        'name': 'Single / not married'
+    },
+    {
+        'id': 'NAME_FAMILY_STATUS_Widow',
+        'name': 'Widow'
+    },
+]
+
+feature_set_3 = [
+    {
+        'id': 'NAME_HOUSING_TYPE_Co-op apartment',
+        'name': 'Co-op apartment',
+    },
+    {
+        'id': 'NAME_HOUSING_TYPE_House / apartment',
+        'name': 'House / apartment',
+    },
+    {
+        'id': 'NAME_HOUSING_TYPE_Municipal apartment',
+        'name': 'Municipal apartment',
+    },
+    {
+        'id': 'NAME_HOUSING_TYPE_Office apartment',
+        'name': 'Office apartment',
+    },
+    {
+        'id': 'NAME_HOUSING_TYPE_Rented apartment',
+        'name': 'Rented apartment',
+    },
+    {
+        'id': 'NAME_HOUSING_TYPE_With parents',
+        'name': 'With parents',
+    },
+]
+
+feature_set_4 = [
+    {
+        'id': 'OCCUPATION_TYPE_Accountants',
+        'name': 'Accountants',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_Cleaning staff',
+        'name': 'Cleaning staff',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_Cooking staff',
+        'name': 'Cooking staff',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_Core staff',
+        'name': 'Core staff',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_Drivers',
+        'name': 'Drivers',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_HR staff',
+        'name': 'HR staff',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_High skill tech staff',
+        'name': 'High skill tech staff',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_IT staff',
+        'name': 'IT staff',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_Laborers',
+        'name': 'Laborers',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_Low-skill Laborers',
+        'name': 'Low-skill Laborers',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_Managers',
+        'name': 'Managers',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_Medicine staff',
+        'name': 'Medicine staff',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_Private service staff',
+        'name': 'Private service staff',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_Realty agents',
+        'name': 'Realty agents',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_Sales staff',
+        'name': 'Sales staff',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_Secretaries',
+        'name': 'Secretaries',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_Security staff',
+        'name': 'Security staff',
+    },
+    {
+        'id': 'OCCUPATION_TYPE_Waiters/barmen staff',
+        'name': 'Waiters/barmen staff',
+    },
+]
